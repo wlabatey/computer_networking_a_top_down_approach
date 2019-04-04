7f3613521e0218a291516d6fc395154f38a4a652
Will Batey <wlabatey@users.noreply.github.com>
2019-04-04 11:07:03 +0100

    Add time.sleep to loop

--- assignments/02_udp/udp_pinger_client.py
+++ assignments/02_udp/udp_pinger_client.py
@@ -3,16 +3,19 @@
 
 import datetime
 import socket
+import time
+
 
 HOST = "127.0.0.1"
 PORT = 12000
 MAX_ATTEMPTS = 10
 SOCKET_TIMEOUT = 1  # Seconds
+LOOP_TIME = .150  # Milliseconds
 
 
 for i in range(MAX_ATTEMPTS):
     t1 = datetime.datetime.now()
-    msg = "Ping {} {}".format(i+1, t1.strftime("%H:%M:%S"))
+    msg = "Ping {}  {}".format(i+1, t1.strftime("%H:%M:%S"))
 
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.settimeout(1)
@@ -25,4 +28,8 @@ for i in range(MAX_ATTEMPTS):
             print("Request {} timed out".format(i+1))
         else:
             delta = (t2 - t1) / datetime.timedelta(microseconds=1000)
-            print("Ping {}   {}   {}ms".format(i+1, t1.strftime("%H:%M:%S"), str(delta)))
+            print("Ping {}  {}  {}ms".format(i+1,
+                                               t1.strftime("%H:%M:%S"),
+                                               str(delta)))
+
+    time.sleep(LOOP_TIME)
